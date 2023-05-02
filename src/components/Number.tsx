569d216013e1bbf8e946277ad6f1993dd130402d
shinya u <shinyahazama7@gmail.com>
2023-05-02 09:48:02 +0900

    first

---
+++ src/components/Number.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../App.css';
+
+function Foo() {
+  return (
+    <div className="App">
+		<div>alsdkjf</div>
+		<div>aaaaaaaaaaaaaaa</div>
+		<div>alsdkjf</div>
+    </div>
+  );
+}
+
+export default Foo;
